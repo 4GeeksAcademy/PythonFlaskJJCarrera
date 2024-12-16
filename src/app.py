f8f67a4f58092e02990fd679ab1e7f86578bddae
JessCP07 <jjcarrera04@gmail.com>
2024-12-16 19:39:51 +0000

    Ejercicios PythonFlask

--- src/app.py
+++ src/app.py
@@ -19,9 +19,9 @@ def get_todos():
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     if position < 0 or position >= len(todos):
-        return jsonify({"error": "Position out of range"}), 404  # Manejo de error si la posición es inválida
-    del todos[position]  # Eliminar el todo en la posición especificada
-    return jsonify(todos)  # Retornar la lista actualizada
+        return jsonify({"error": "Position out of range"}), 
+    del todos[position]  
+    return jsonify(todos)  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
